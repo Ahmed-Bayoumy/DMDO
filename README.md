759b7178bb37db27bdc276eadc49fa9377f8a29f
Ahmed Bayoumy <ahmed.bayoumy@mail.mcgill.ca>
2026-02-14 11:45:56 -0500

    License updates

--- README.md
+++ README.md
@@ -13,13 +13,18 @@ For technical and code documentation, please visit [DMDO Webpage](https://ahmed-
 
 ---
 
-**Version 1.3.0**
+**Version 2601.0**
 
 ---
 
 ## License & copyright
 
 © Ahmed H. Bayoumy 
+
+This project is licensed under the GNU General Public License v3.0 or later (GPL-3.0-or-later).
+
+See the LICENSE file for details.
+
 ---
 
 ## Citation
--- src/DMDO/DA.py
+++ src/DMDO/DA.py
@@ -1,3 +1,4 @@
+"""
 # ------------------------------------------------------------------------------------#
 #  Distributed Multidisciplinary Design Optimization - DMDO                           #
 #                                                                                     #
@@ -5,20 +6,21 @@
 #  email: ahmed.bayoumy@mail.mcgill.ca                                                #
 #                                                                                     #
 #  This program is free software: you can redistribute it and/or modify it under the  #
-#  terms of the GNU General Public License as published by the Free Software   #
+#  terms of the GNU General Public License as published by the Free Software          #
 #  Foundation, either version 3 of the License, or (at your option) any later         #
 #  version.                                                                           #
 #                                                                                     #
 #  This program is distributed in the hope that it will be useful, but WITHOUT ANY    #
 #  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A    #
-#  PARTICULAR PURPOSE.  See the GNU General Public License for more details.   #
+#  PARTICULAR PURPOSE.  See the GNU General Public License for more details.          #
 #                                                                                     #
-#  You should have received a copy of the GNU General Public License along     #
+#  You should have received a copy of the GNU General Public License along            #
 #  with this program. If not, see <http://www.gnu.org/licenses/>.                     #
 #                                                                                     #
 #  You can find information on simple_mads at                                         #
 #  https://github.com/Ahmed-Bayoumy/DMDO                                              #
 # ------------------------------------------------------------------------------------#
+# """
 
 import copy
 from genericpath import isfile
