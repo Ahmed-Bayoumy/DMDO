574b306b5d58049ece4545ea9fc16be9d7ad6aaf
Ahmed-Bayoumy <ahmed.bayoumy@mail.mcgill.ca>
2026-03-29 19:51:53 -0400

    fix ruff issue

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -217,7 +217,7 @@ def test_basic_MDO():
   )
 
   # Run the MDO problem
-  p_file: str = os.path.abspath("./tests/test_files/Basic_MDO.out")
+  # p_file: str = os.path.abspath("./tests/test_files/Basic_MDO.out")
   MDAO.run(mode="serial")
 
   print('------Run_Summary------')
